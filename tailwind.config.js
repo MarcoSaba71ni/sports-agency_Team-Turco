cbf1513822608f9d7466b0360ba3953897f7702b
Marco Sabatini <marsab04188@stud.noroff.no>
2026-01-20 00:37:23 -0300

    Initial project setup with Tailwind and components
    Added initial project files including Tailwind CSS setup, header and footer HTML components, main index.html, and supporting assets. Configured VS Code live server port and included required dependencies in package.json and package-lock.json.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./**/*.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'black',
+        secondary: 'white',
+        tertiary: '#959595',
+        quaternary: '#0D6868',
+      }
+    },
+  },
+  plugins: [],
+}
