96b45309b14bc2e7809a551e7a6f9e148cfc082d
Marco Sabatini <marsab04188@stud.noroff.no>
2026-01-22 00:31:23 -0300

    Creating two different header divs for desktop and mobile versions

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
 const dropdownBtn = document.getElementById('dropdown-btn');
 const mobileNav = document.getElementById('mobile-nav');
 
-dropdownBtn.addEventListener('click', () => {
-    mobileNav.classList.toggle('hidden');
+const languageBtn = document.getElementById('lang-btn');
+const languageNav = document.getElementById('lang-nav');
+
+
+// Toggle mobile nav
+dropdownBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  mobileNav.classList.toggle("hidden");
+  languageNav.classList.add("hidden");
+});
+
+// Toggle language menu
+languageBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  languageNav.classList.toggle("hidden");
+  mobileNav.classList.add("hidden");
+});
+
+// Close when clicking outside
+document.addEventListener("click", (e) => {
+  if (
+    !mobileNav.contains(e.target) &&
+    !dropdownBtn.contains(e.target)
+  ) {
+    mobileNav.classList.add("hidden");
+  }
+
+  if (
+    !languageNav.contains(e.target) &&
+    !languageBtn.contains(e.target)
+  ) {
+    languageNav.classList.add("hidden");
+  }
 });
