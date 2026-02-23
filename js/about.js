f063319eb3118626c1b2cae7f5a7980f06a09716
Marco Sabatini <marsab04188@stud.noroff.no>
2026-02-22 23:03:33 -0300

    more changes on about page

--- js/about.js
+++ js/about.js
@@ -11,3 +11,18 @@ setInterval(() => {
 
     dynamicText.textContent = "YOUR " + words[index];
 }  , 1000);
+
+const teamSection = document.getElementById("trainers-profiles");
+const showMoreBtn = document.getElementById("show-more-btn_profile");
+const showLessBtn = document.getElementById("show-less-btn_profile");
+
+showMoreBtn.addEventListener("click", () => {
+    teamSection.classList.remove("hidden");
+    teamSection.scrollIntoView({ behavior: "smooth" },2000);
+    showMoreBtn.classList.add("hidden");
+});
+
+showLessBtn.addEventListener("click", () => {
+    teamSection.classList.add("hidden");
+    showMoreBtn.classList.remove("hidden");
+});
