645310b53d5a65b79335893fdbd140129ed12d54
Marco Sabatini <marsab04188@stud.noroff.no>
2026-04-10 14:36:38 +0200

    updating index, about and athletes page in order to match client requirements

--- js/athletes.js
+++ js/athletes.js
@@ -4,3 +4,26 @@ exploreBtn.addEventListener('click', () => {
     console.log('Explore button clicked');
     window.location.href = 'high-performance.html';
 });
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const carousel = document.getElementById("athletes-carousel");
+  const nextBtn = document.getElementById("next-btn");
+  const prevBtn = document.getElementById("prev-btn");
+
+  const scrollAmount = 300;
+
+  nextBtn.addEventListener("click", () => {
+    carousel.scrollBy({
+      left: scrollAmount,
+      behavior: "smooth"
+    });
+  });
+
+  prevBtn.addEventListener("click", () => {
+    carousel.scrollBy({
+      left: -scrollAmount,
+      behavior: "smooth"
+    });
+  });
+});
