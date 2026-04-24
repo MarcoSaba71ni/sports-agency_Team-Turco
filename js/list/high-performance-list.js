1a96658ee0ecde15258a421a7f76d12ad04a0dd2
Marco Sabatini <marsab04188@stud.noroff.no>
2026-04-24 13:55:39 +0200

    more changes

--- js/list/high-performance-list.js
+++ js/list/high-performance-list.js
@@ -26,6 +26,32 @@ export const highPerformanceAthletes = [
     }
   },
   {
+    name: "Gean Miller",
+    sport: "Football",
+    image: {
+      src: "../images/Athletes-TeamTurco/gean-miller.jpeg",
+      alt: "Gean Miller"
+    },
+    age: 28,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
+    name: "Bruno dal Maso ",
+    sport: "Basketball",
+    image: {
+      src: "../images/Athletes-TeamTurco/bruno-playing-basketball.jpeg",
+      alt: "Bruno Dal Maso"
+    },
+    age: 31,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
     name: "Joao Pierre",
     sport: "Swimming",
     image: {
@@ -39,19 +65,18 @@ export const highPerformanceAthletes = [
     }
   },
   {
-    name: "Gean Miller",
+    name: "Lukinhas",
     sport: "Football",
     image: {
-      src: "../images/Athletes-TeamTurco/gean-miller.jpeg",
-      alt: "Gean Miller"
+      src: "../images/Athletes-TeamTurco/lukas-football (2).jpeg",
+      alt: "Lukinhas"
     },
-    age: 28,
+    age: 27,
     flag: {
       src: "../images/flags/Flag_of_Brazil.svg",
       alt: "Brazil flag"
     }
   },
-  
   {
     name: "Marco Sabatini",
     sport: "Football",
@@ -64,5 +89,70 @@ export const highPerformanceAthletes = [
       src: "../images/flags/Flag_of_Brazil.svg",
       alt: "Brazil flag"
     }
-  }
+  },
+  {
+    name: "Jusep",
+    sport: "Marathon",
+    image: {
+      src: "../images/Athletes-TeamTurco/jaime-ironman.jpeg",
+      alt: "Jusep"
+    },
+    age: 32,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
+    name: "Joao Gomes",
+    sport: "Football",
+    image: {
+      src: "../images/Athletes-TeamTurco/joao-gomes (1).jpeg",
+      alt: "Joao Gomes"
+    },
+    age: 28,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
+    name: "Vinicius ",
+    sport: "Paddle Sup",
+    image: {
+      src: "../images/Athletes-TeamTurco/vinicius-paddlesup (2).jpeg",
+      alt: "Vinicius"
+    },
+    age: 62,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
+    name: "Victor Particelli",
+    sport: "Swimming",
+    image: {
+      src: "../images/Athletes-TeamTurco/victor-swimmer.jpeg",
+      alt: "Victor Particelli"
+    },
+    age: 23,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
+  {
+    name: "Magdiel",
+    sport: "Football",
+    image: {
+      src: "../images/Players-TeamTurco/Magdiel_img01.jpeg",
+      alt: "Magdiel"
+    },
+    age: 32,
+    flag: {
+      src: "../images/flags/Flag_of_Brazil.svg",
+      alt: "Brazil flag"
+    }
+  },
 ];
